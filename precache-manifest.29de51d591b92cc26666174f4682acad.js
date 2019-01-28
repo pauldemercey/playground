161ae81e1c556999882a6b4352067bb31b5a7bad
Paul de Mercey <pmercey@gilt.jp>
2019-01-28 13:10:44 +0900

    Updates

---
+++ precache-manifest.29de51d591b92cc26666174f4682acad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/playground/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fd2222a613df36f2a7bd",
+    "url": "/playground/static/js/runtime~main.fd2222a6.js"
+  },
+  {
+    "revision": "b757dedf22892a1905aa",
+    "url": "/playground/static/js/main.b757dedf.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/playground/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "b757dedf22892a1905aa",
+    "url": "/playground/static/css/main.30e226f6.chunk.css"
+  },
+  {
+    "revision": "94b3229386db52a047faad24e10f0b10",
+    "url": "/playground/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.45e76107f64c11761a3865ca17d28853.js"
+  "/playground/precache-manifest.29de51d591b92cc26666174f4682acad.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/playground/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
